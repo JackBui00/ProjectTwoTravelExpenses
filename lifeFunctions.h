3fa8c4b73b70aeb0f4eb8844cfbcd327695241ad
Jack Bui <jdbui@cpp.edu>
2021-09-22 16:43:18 -0700

    Create lifeFunctions file

---
+++ lifeFunctions.h
@@ -0,0 +1,15 @@
+#ifndef __LIFEFUNCTIONS_H_
+#define __LIFEFUNCTIONS_C_
+#include <stdio.h>
+
+
+
+int accommodationCost(){
+    int totalCost; 
+    printf("How much was the total cost of housing? ");
+    scanf("%d",&totalCost); 
+    return totalCost; 
+}
+
+
+#endif
