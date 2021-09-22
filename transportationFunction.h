83f09db64bf79bac6e14b6ea9276d03d60feb311
Jack Bui <jdbui@cpp.edu>
2021-09-22 16:44:47 -0700

    grammar and spacing fix

--- transportationFunction.h
+++ transportationFunction.h
@@ -4,7 +4,7 @@
 double privateCarCost(){
     int miles; 
     //request how many miles were driven 
-    printf("How many miles were driven in the rented vehicle?");
+    printf("How many miles were driven in the rented vehicle? ");
     scanf("%d", &miles);
     double privateCarTotal;
     //Muliplies miles by cost per mile 
@@ -14,7 +14,7 @@ double privateCarCost(){
 double taxiCost(){
     double taxiCost; 
     //request total cost of taxi services 
-    printf("How much was the total cost of the taxi services?");
+    printf("How much was the total cost of the taxi services? ");
     scanf("%lf", &taxiCost); 
     return taxiCost; 
 }
