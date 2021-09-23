a5fc3f832204c9e9fa93ead74226514d8aa8df6c
Alvin Le <alvinhle@cpp.edu>
2021-09-23 16:52:29 -0700

    Added cost of airfare

---
+++ airfare.h
@@ -0,0 +1,18 @@
+#ifndef _AIRFARE_H_
+#define _AIRFARE_C_
+#include <stdio.h>
+
+int airfare(){
+    
+    //asks and returns the total cost of airfare
+    int airfareCost;
+    scanf("%d", airfareCost);
+    return airfareCost;
+
+}
+
+
+
+
+
+#endif
