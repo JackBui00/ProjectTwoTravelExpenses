24fb8c81e2bf5db13ed157e33a88c6342db099fe
Jack Bui <jdbui@cpp.edu>
2021-09-22 17:13:27 -0700

    add function to calculate business covered breakfast amount

--- lifeFunctions.h
+++ lifeFunctions.h
@@ -6,10 +6,28 @@
 
 int accommodationCost(){
     int totalCost; 
+    //request total cost of hotels
     printf("How much was the total cost of housing? ");
     scanf("%d",&totalCost); 
     return totalCost; 
 }
 
-
+int calculateBreakfast(int days, int departure, int arrival){
+    int businessBreakfast = days;
+    if(departure<700 && arrival >800){
+        return days; 
+    }
+    else if(departure>700 && arrival <800){
+        return businessBreakfast-=2; 
+    }
+    else{
+        return businessBreakfast-=1; 
+    }
+}
+int calculateLunch(int days, int departure, int arrival){
+    int businesslunch = days;
+}
+int calculateDinner(int days, int departure, int arrival){
+    int businessdinner = days;
+}
 #endif
