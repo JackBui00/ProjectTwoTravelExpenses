4c46fb3afd680fcb5c1c315d540649db6ffd01bb
Jack Bui <jdbui@cpp.edu>
2021-09-22 17:19:02 -0700

    add comments for calculateBreakfast function

--- lifeFunctions.h
+++ lifeFunctions.h
@@ -14,18 +14,22 @@ int accommodationCost(){
 
 int calculateBreakfast(int days, int departure, int arrival){
     int businessBreakfast = days;
+    //If the time of departure is after 7am, and the time of arrival home is after 8am, both the first day and last day's breakfast is covered by the business
     if(departure<700 && arrival >800){
         return days; 
     }
+    // if the departure falls past 7pm and the time of arrival home is before 8am, breakfast on the first and last day are not covered by the business. 
     else if(departure>700 && arrival <800){
         return businessBreakfast-=2; 
     }
+    // if both breakfast were not covered by the business, but both weren't covered by the business, one was covered by the business. 
     else{
         return businessBreakfast-=1; 
     }
 }
 int calculateLunch(int days, int departure, int arrival){
     int businesslunch = days;
+
 }
 int calculateDinner(int days, int departure, int arrival){
     int businessdinner = days;
