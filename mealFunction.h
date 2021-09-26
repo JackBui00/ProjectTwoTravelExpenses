56d50f59ad67c422ccd3d98b148eee5c8bb1d283
TeymoorianSisian <89603308+TeymoorianSisian@users.noreply.github.com>
2021-09-25 19:13:47 -0700

    Update mealFunction.h

--- mealFunction.h
+++ mealFunction.h
@@ -1,26 +1,26 @@
 #ifndef __MEALFUNCTION_H_
 #define __MEALFUNCTION_C_
 
-double mealExpense() {
-	char choice;
-	char choice1;
-	double mealCost;
-	int time;
-	int time2;
-	double totalCost;
-	double total_cost_meal;
-	double total_allowable_cost;
-	const double allowable_breakfast_cost = 9.00,
-                      allowable_lunch_cost= 12.00,
-                      allowable_dinner_cost = 16.00;
-	printf("\nDid you eat meal on the first day? (Y or N): ");
-	scanf("%d", &choice);
-	printf("\nWhat time did you eat your meal? ");
-	scanf("%d", &time);
-	
-	printf("\nDid you eat a meal on the last day? (Y or N) ";
-    	scanf("%d", choice1);
-	printf("\nWhat time did you eat your meal? ");
-	scanf("%d", time2); 
+double breakfastCost() {
+	double breakfastCost;
+	printf("How much did you spend on breakfast? ");
+	scanf("%lf", &breakfastCost);
+	return breakfastCost;
+}
+#endif 
+
+double lunchCost() {
+	double lunchCost;
+	printf("How much did you spend on lunch? ");
+	scanf("%lf", &lunchCost);
+	return lunchCost;
+}
+#endif 
+
+double dinnerCost() {
+	double dinnerCost;
+	printf("How much did you spend on dinner? ");
+	scanf("%lf", &dinnerCost);
+	return dinnerCost;
 }
 #endif
