37c935a9bc3494acb05924f03e1bfe67cc626a6a
TeymoorianSisian <89603308+TeymoorianSisian@users.noreply.github.com>
2021-09-25 18:27:15 -0700

    Update main.c

--- main.c
+++ main.c
@@ -69,112 +69,19 @@ int main(){
         int totalHotelCost = accommodationCost(); 
 
         //Food needs
-        double inputValidate(double); 
-        double inputValidate(double, int)
-	char choice;
-	char choice1;
-	double mealCost;
-	int time;
-	int time2;
-	double totalCost;
-	double total_cost_meal;
-	double total_allowable_cost;
-	const double allowable_breakfast_cost = 9.00,
-                      allowable_lunch_cost= 12.00,
-                      allowable_dinner_cost = 16.00;
-	double total_meal_cost;
-        printf("\nDid you eat meal on the first day? (Y or N): ");
-	scanf("%d", &choice);
-	printf("\nWhat time did you eat your meal? ");
-	scanf("%d", &time);
-	if (choice =='Y' || choice == 'y')
-	    {
-		printf("How much was your meal? ");
-		mealCost = inputValidate(mealCost, 0);
-                total_meal_Cost += mealCost;
+	char allowable_breakfast_cost[9];
+	int totalbreakfastCost = 0;
+	int paid_by employee = 0; 
+	printf("Do you eat the breakfast on the first day?(Y/N) )";
+	scanf("%s", &allowable_breakfast_cost);
+	int result = strcmp("yes", allowable_breakfast_cost); 
 
-		if(time < 7)
-        		{
-			if (mealCost > allowable_breakfast_cost) // 15 > 9 = 6.00
-                			total_meal_cost = mealCost - allowable_breakfast_cost;
-
-			else if (mealCost <= allowable_breakfast_cost) // 5 < 9 
-                			total_meal_cost = allowable_breakfast_cost - mealCost ; //saving 
-            
-		}
-		
-		else if (time >= 7 && time < 12)
-        		{
-            		total_allowable_cost = allowable_lunch_cost;
-
-            		if (mealCost > allowable_lunch_cost) // 14 > 12 = $2.00
-				total_meal_cost = mealCost - allowable_lunch_cost;
-
-            		else if (mealCost <= allowable_lunch_cost) // 10 < 12 = 2
-                			total_meal_cost = allowable_lunch_cost - mealCost;
-
-        		}
-		else if (time >= 12 && time < 18)
-        		{
-            		total_allowable_cost = allowable_dinner_cost;
-
-            		if (mealCost > allowable_dinner_cost)
-                			total_meal_cost = mealCost - allowable_dinner_cost;
-
-            		else if (mealCost <= allowable_dinner_cost)
-                			total_meal_cost = allowable_dinner_cost - mealCost;
-        		}
-
-        		else if (time >= 18)
-            		total_meal_cost = mealCost;
-	} 
-	printf("\nDid you eat a meal on the last day? (Y or N) ");
-    	scanf("%d", &choice1);
-	printf("\nWhat time did you eat your meal? ");
-	scanf("%d", time2); 
-	if (choice1 == 'Y' || choice1 == 'y')
-    	{
-		printf("\nHow much was your meal? ");
-        		scanf("%d", &mealCost);
-
-        		totalCost = mealCost;
-
-        		if(time2 <= 8)
-            		total_meal_cost = mealCost;
-
-        		else if(time2 > 8 && time2 <= 13)
-        		{
-            		total_allowable_cost = allowable_breakfast_cost;
-
-            		if (mealCost> allowable_breakfast_cost)
-                			total_meal_cost = mealCost - allowable_breakfast_cost;
-
-            		else if (mealCost <= allowable_breakfast_cost)
-                			total_meal_cost = mealCost;
-        		}
-
-       		else if (time2 > 13 && time2 <= 19)
-        		{
-            		total_allowable_cost = allowable_lunch_cost;
-
-            		if (mealCost > allowable_lunch_cost) // 13.00 > 12.00 == 1.00
-                			total_meal_cost = mealCost - allowable_lunch_cost;
-
-            		else if (mealCost <= allowable_lunch_cost)
-                			total_meal_cost = mealCost; // 12.00 < n;
-        		}
-
-        		else if (time2 > 19)
-        		{
-            		total_allowable_cost += allowable_dinner_cost;
-            		if (mealCost > allowable_dinner_cost)
-                			total_meal_cost = mealCost - allowable_dinner_cost;
-
-            		else if (mealCost<= allowable_dinner_cost)
-                			total_meal_cost = mealCost;
-        		}
-        total_meal_cost = mealExpense();    
-    }
+	if(result == 0) {
+		printf("How much was your breakfast? ");
+		scanf("%d", &breakfast_cost);
+		paid_by employee = breakfastCost - 9;
+		totalbreakfastCost = breakfastCost();
+}
 
 
 
