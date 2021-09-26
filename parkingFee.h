55c61dcdc72a0dac514d9cf384774cd232c40a73
ericaatann <89226691+ericaatann@users.noreply.github.com>
2021-09-25 22:44:29 -0700

    Create parkingFee.h

---
+++ parkingFee.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#ifndef __TRANSPORTATIONFUNCTION_H_
+#define __TRANSPORTATIONFUNCTION_C_
+
+double ParkingFee(int daysOfTrip, double parking, double totalExpenses,
+        double reimbursement, double amntSaved, double totalAllowableExpense){
+    char input;
+    printf("Was there any parking fees? ")
+    scanf("%c", &input);
+
+    
+    
+    if(input == 'y' || input == 'Y'){
+        double allowableFee = 6.00;
+        double parkingFee;
+        
+        for (int i = 0; i < daysOfTrip; i++){
+            totalAllowableExpense += allowableFee;
+
+            printf("Enter parking fees for the day : ")
+            scanf("%d", &parkingFee);
+
+            
+            totalExpenses += parkingFee;
+
+            if (parkingFee > allowableFee){
+                reimbursement += (parkingFee - allowableFee);
+            }
+            else if(parkingFee <= allowableFee){
+                amntSaved += parkingFee;
+            }
+        }
+    }
+    return 0;
+}
+#endif
